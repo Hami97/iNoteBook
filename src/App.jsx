f2de0e628ec2896e51c0bea2a6563f2bca7a2c1a
Muhammad Hamza <zafarhamza652@gmail.com>
2025-02-01 03:59:09 +0500

    Till 58

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,24 @@ import { Router as Router, Switch, Route } from "wouter";
 import { Navbar } from "./components/Navbar";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
+import NoteState from "./context/NoteState";
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-        </Switch>
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </Router>
+      </NoteState>
     </>
   );
 }
