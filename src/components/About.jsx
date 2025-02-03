7a8176611403413a99deac71fbb5d532fab2b79f
Muhammad Hamza <zafarhamza652@gmail.com>
2025-02-04 03:09:31 +0500

    Till 59

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,15 +2,5 @@ import React from "react";
 import { useContext, useEffect } from "react";
 import noteContext from "../context/noteContext";
 export const About = () => {
-  const a = useContext(noteContext);
-  useEffect(() => {
-    a.update();
-    //esline-disable-next-line
-  }, []);
-
-  return (
-    <div>
-      This is About {a.state.name} and he is in {a.state.class}
-    </div>
-  );
+  return <div>This is About Page</div>;
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { Link } from "wouter";
+import { Link, useLocation } from "wouter";
+import { useEffect } from "react";
 
 export const Navbar = () => {
+  let location = useLocation();
+  useEffect(() => {
+    console.log(location.pathname);
+  }, [location]);
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -26,12 +31,23 @@ export const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === "/" ? "active" : ""
+                  }`}
+                  aria-current="page"
+                  to="/"
+                >
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === "/about" ? "active" : ""
+                  }`}
+                  to="/about"
+                >
                   About
                 </Link>
               </li>
