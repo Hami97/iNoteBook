63da14bfee452859aa2da4409db851380d0cb568
Muhammad Hamza <zafarhamza652@gmail.com>
2025-02-11 01:02:40 +0500

    Till 63

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,16 +1,21 @@
 import React, { useContext } from "react";
 import { Noteitem } from "./NoteItem.jsx";
 import noteContext from "../context/noteContext.jsx";
+import { AddNote } from "./AddNote";
 
 export const Notes = () => {
   const context = useContext(noteContext);
-  const { notes, setNotes } = context;
+  const { notes, addNote } = context;
   return (
-    <div className="row my-3">
-      <h4>Your Notes</h4>
-      {notes.map((note) => (
-        <Noteitem note={note} key={note.id || note._id} />
-      ))}
-    </div>
+    <>
+      <AddNote />
+      <hr />
+      <div className="row my-3">
+        <h4>Your Notes</h4>
+        {notes.map((note) => (
+          <Noteitem note={note} key={note.id || note._id} />
+        ))}
+      </div>
+    </>
   );
 };
--- src/context/NoteState.jsx
+++ src/context/NoteState.jsx
@@ -67,20 +67,40 @@ const NoteState = (props) => {
       "date": "2025-01-28T18:43:17.831Z",
       "__v": 0,
     },
-    {
-      "_id": "67992545a20b106bd400ac50",
-      "user": "67916914805d0643621f17a2",
-      "title": "New Note",
-      "description": "Hello.",
-      "tag": "General",
-      "date": "2025-01-28T18:43:17.831Z",
-      "__v": 0,
-    },
   ];
 
   const [notes, setNotes] = useState(notesInitial);
+
+  //Add a Note
+  const addNote = (title, description, tag) => {
+    //TODO: API call
+
+    const note = {
+      "_id": new Date().getTime().toString(),
+      "user": "67916914805d0643621f17a2",
+      "title": title,
+      "description": description,
+      "tag": tag,
+      "date": new Date().toISOString(),
+      "__v": 0,
+    };
+    setNotes([...notes, note]);
+    //setNotes(notes.concat(note));
+  };
+
+  //Delete a Note
+  const deleteNote = () => {};
+
+  //Edit a Note
+  const editNote = () => {};
+
+  //View a note
+  const viewNote = () => {};
+
   return (
-    <NoteContext.Provider value={{ notes, setNotes }}>
+    <NoteContext.Provider
+      value={{ notes, setNotes, addNote, deleteNote, editNote }}
+    >
       {props.children}
     </NoteContext.Provider>
   );
